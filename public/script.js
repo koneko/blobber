61e4387c907b106b3427de3ad8bd4a1b83e889df
koneko <67551503+koneko@users.noreply.github.com>
2022-06-09 18:58:47 +0200

    dear god please help me fix this mess

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+import { Camera, PanTo } from './camera.js'
 const app = new PIXI.Application()
 document.querySelector("div.canvas").appendChild(app.view);
 // set app width and height to be the same as the window
@@ -12,9 +13,18 @@ class Game {
         this.state = {}
         this.elements = []
         this.playerName = null
+        this.world = null
+        this.player = null
+    }
+    createCamera () {
+        this.camera = new Camera({ ticker: app.ticker })
     }
     createWorld () {
-        this.world = new World()
+        this.world = new World(this)
+    }
+    createPlayer () {
+        this.player = new Player(this)
+        this.player.draw()
     }
     changeState (inputstate) {
         this.state.name = inputstate
@@ -44,6 +54,8 @@ class Game {
                 app.stage.removeChild(element.text)
             } else app.stage.removeChild(element)
         })
+        //remove all elements
+        this.elements = []
     }
     promptName () {
         let name = prompt("Username?", "An unnamed blobber")
@@ -58,6 +70,8 @@ class Game {
         let button = new PIXI.Graphics()
         button.beginFill(0xffffff)
         button.drawRect(x, y, width, height)
+        button.lineStyle(2, 0x000000)
+        button.drawRect(x, y, width, height)
         button.endFill()
         button.interactive = true
         button.buttonMode = true
@@ -65,15 +79,12 @@ class Game {
         let textObject = new PIXI.Text(text, {
             fontFamily: "Lotion",
             fontSize: 30,
-            fill: 0x000000,
-            align: "center"
+            fill: 0x000000
         })
         //center text in the button
         textObject.x = x + width / 2 - textObject.width / 2
         textObject.y = y + height / 2 - textObject.height / 2
         //add border to button
-        button.lineStyle(2, 0x000000)
-        button.drawRect(x, y, width, height)
         app.stage.addChild(button)
         app.stage.addChild(textObject)
         let buttonObj = {
@@ -107,47 +118,84 @@ class Game {
             this.changeState("game")
             this.runState()
         })
+        this.createButton("Set username", buttonX, buttonY + buttonHeight + 30, buttonWidth, buttonHeight, () => { this.promptName() })
     }
     game () { // game state
         console.log("game state active")
-        if (this.playerName == null) this.promptName()
+        if (this.playerName == null) return this.changeState("main")
+        if (this.state.name != "game") return
         this.clearElements()
+        this.createCamera()
+        this.createWorld()
+        this.world.createBackground()
+        this.createPlayer()
+        this.world.pan(this.player.x, this.player.y)
     }
 }
 
 class World {
-    constructor () {
+    constructor (game) {
         //set width and height to browser window size
-        this.width = 1000
-        this.height = 1000
+        this.game = game
+        this.width = "1000px"
+        this.height = "1000px"
+        this.elements = game.elements
+        this.camera = game.camera
+    }
+    createBackground () {
+        //load background image and infinitely tile it
+        //background url /assets/background.jpg
+        let texture = PIXI.Texture.from("/assets/background.jpg")
+        let background = new PIXI.TilingSprite(texture, 10000, 10000)
+        //background.width = 1000
+        //background.height = 1000
+        //make the background tile infinitely
+        background.tileScale.x = 1
+        background.tileScale.y = 1
+        background.tilePosition.x = 0
+        background.tilePosition.y = 0
+        app.stage.addChild(background)
+        this.elements.push(background)
+        console.log(`Drawing background W: ${background.width} H: ${background.height}`)
+    }
+    pan (x, y) {
+        console.log(`Panning camera to X: ${x} Y: ${y}`)
+        let pan = new PanTo(app.stage, x - app.renderer.width / 2, y - app.renderer.height / 2, 5000);
+        this.camera.effect(pan)
     }
 }
 
 class Player {
-    constructor () {
+    constructor (game) {
+        this.game = game
         this.mass = 100
         this.radius = this.calculateRadius()
         this.speed = this.calculateSpeed()
-        //randomly choose x and y coordinates within the world
-        this.x = Math.random() * 1000
-        this.y = Math.random() * 1000
+        //randomly choose x and y coordinates within the world from 0 to world width and height
+        this.x = random(0, +game.world.width.replace("px", ""))
+        this.y = random(0, +game.world.height.replace("px", ""))
         //randomly picked color
         this.color = Math.random() * 0xffffff
+        this.draw()
     }
-    draw (x, y) {
+    draw () {
         //draw circle at x and y 
+        console.log(`Drawing player X: ${this.x} Y: ${this.y} Radius: ${this.radius}`)
         this.circle = new PIXI.Graphics()
         let circle = this.circle
         circle.beginFill(this.color)
-        circle.drawCircle(x, y, this.radius)
+        circle.drawCircle(this.x, this.y, this.radius)
+        //give z index higher than background
+        circle.zIndex = 1
         circle.endFill()
         app.stage.addChild(circle)
+        this.game.elements.push(circle)
     }
     calculateRadius () {
-        return Math.sqrt(this.mass / Math.PI)
+        return this.mass / 3
     }
     calculateSpeed () {
-        return Math.sqrt(Math.pow(this.radius + 2))
+        return Math.sqrt(Math.pow(this.radius + 2, 2))
     }
     moveX (velocity) {
         this.x += velocity * this.speed
@@ -155,8 +203,16 @@ class Player {
     moveY (velocity) {
         this.y += velocity * this.speed
     }
+    add (mass) {
+        this.mass += mass
+        this.radius = this.calculateRadius()
+        this.speed = this.calculateSpeed()
+    }
 }
-
-const game = new Game()
-game.changeState("main")
-game.runState()
+window.onresize = () => {
+    app.renderer.resize(window.innerWidth, window.innerHeight)
+}
+window.game = new Game()
+window.game.changeState("main")
+window.game.runState()
+window.game.playerName = "Koneko"
